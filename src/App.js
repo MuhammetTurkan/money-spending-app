674260e6588173fd19bfd79c82fd2fb18e584054
Muhammet Türkan <83230346+MuhammetTurkan@users.noreply.github.com>
2022-06-12 15:51:01 +0300

    added products and added buy and sell buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import products from "./components/products.json";
+import Product from "./components/Product";
 
 function App() {
+  const [money, setMoney] = useState(100);
+  const [basket, setBasket] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const ttl = basket.reduce((acc, item) => {
+      return (
+        acc +
+        item.amount * products.find((product) => product.id === item.id).price
+      );
+    }, 0);
+    console.log(ttl);
+  }, [basket]);
+
   return (
-    <div className="App">
-      <>test</>
-    </div>
+    <>
+      <Header money={money} />
+      {products.map((product) => (
+        <Product
+          basket={basket}
+          setBasket={setBasket}
+          product={product}
+          key={product.id}
+        />
+      ))}
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header({ money }) {
+  return <div>Harcamak için {money} $ var !</div>;
+}
+
+export default Header;
